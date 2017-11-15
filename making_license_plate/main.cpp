58704b52d2cb84c3fd4eb207a25b60439286a07a
ssilb4 <ssilb4@gmail.com>
2017-11-15 16:56:05 +0900

    Update main.cpp

--- making_license_plate/main.cpp
+++ making_license_plate/main.cpp
@@ -1,8 +1,11 @@
 #include "makexml.h"
 
-const int max_num = 2;
-const int min_num = 1;
-const int max_char = 4;
+const int max_num = 1000;	//만들 개수
+const int min_num = 0;		//고정
+const int car_count = 10;	//고정
+const int back_count = 10;	//고정
+const int num_count = 10;	//고정
+const int char_count = 33;	//고정
 const string imgtype = ".jpg";
 const string txttype = ".txt";
 const string plate = "plate";
@@ -21,6 +24,8 @@ const int char_y = 40;
 cv::Size plate_size = cv::Size(172, 37);
 int plate_x[2] = { 264,305 };
 int plate_y[2] = { 264,305 };
+int result_x[2] = { 264,305 };
+int result_y[2] = { 264,305 };
 
 void file_read(string name, string type) {
 	ifstream ifile;
@@ -58,106 +63,122 @@ int main()
 	string result_name = result_directory;
 	string temp_char = directory + character[0] + imgtype;
 	string temp_num = directory + number[0] + imgtype;
-	
+
 
 	cv::Mat image = cv::imread(temp_plate);
 	cv::Mat logo_char = cv::imread(temp_char);
 	cv::Mat logo_num = cv::imread(temp_num);
-	for(int back_index = 0; back_index<10; back_index++){
+
+	//랜덤
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937_64 ranNum(seed);
+
+	for (int index = min_num; index < max_num; index++) {
+		//background
+		int back_index = ranNum() % back_count;
 		string temp_back = directory + background + '/' + background + number[back_index];
 		cv::Mat background_image = cv::imread(temp_back + imgtype);
 
-		for (int car_index = min_num; car_index < 10; car_index++) {
-			string temp_car = directory + car + '/' + car + number[car_index];
-			cv::Mat car_image = cv::imread(temp_car + imgtype);
-			file_read(temp_car, txttype);
-			for (int a = min_num; a < max_num; a++) {
-				temp_num = directory + number[a] + imgtype;
-				logo_num = cv::imread(temp_num);
-				cv::Mat imageROI = image(cv::Rect(char_x[0], char_y, logo_num.cols, logo_num.rows));
-				cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
-				for (int b = min_num; b < max_num; b++) {
-					temp_num = directory + number[b] + imgtype;
-					logo_num = cv::imread(temp_num);
-					imageROI = image(cv::Rect(char_x[1], char_y, logo_num.cols, logo_num.rows));
-					cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
-					for (int c = min_num; c < max_char; c++) {
-						temp_char = directory + character[c] + imgtype;
-						logo_char = cv::imread(temp_char);
-						imageROI = image(cv::Rect(char_x[2], char_y, logo_char.cols, logo_char.rows));
-						cv::addWeighted(imageROI, 0.0, logo_char, 1.0, 0., imageROI);
-						for (int d = min_num; d < max_num; d++) {
-							temp_num = directory + number[d] + imgtype;
-							logo_num = cv::imread(temp_num);
-							imageROI = image(cv::Rect(char_x[3], char_y, logo_num.cols, logo_num.rows));
-							cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
-							for (int e = min_num; e < max_num; e++) {
-								temp_num = directory + number[e] + imgtype;
-								logo_num = cv::imread(temp_num);
-								imageROI = image(cv::Rect(char_x[4], char_y, logo_num.cols, logo_num.rows));
-								cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
-								for (int f = min_num; f < max_num; f++) {
-									temp_num = directory + number[f] + imgtype;
-									logo_num = cv::imread(temp_num);
-									imageROI = image(cv::Rect(char_x[5], char_y, logo_num.cols, logo_num.rows));
-									cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
-									for (int g = min_num; g < max_num; g++) {
-										temp_num = directory + number[g] + imgtype;
-										logo_num = cv::imread(temp_num);
-										imageROI = image(cv::Rect(char_x[6], char_y, logo_num.cols, logo_num.rows));
-										// 영상 ROI 정의
-										// 사각형으로 시작x 시작y 끝x 끝y
-										cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
-										// 영상에 로고 붙이기
-										// imageROI = 1.0*imageROI + 0.3*logo + 0
-										result_name = number[a] + number[b] + character[c] + number[d] + number[e] + number[f] + number[g] + car + char(car_index + '0') + background + char(back_index + '0'); //car number back number
-										//cv::imshow("aaa", image);
-										cout << result_name << endl;
-										//cv::imwrite("abc.jpg", image);
+		//car
+		int car_index = ranNum() % car_count;
+		string temp_car = directory + car + '/' + car + number[car_index];
+		cv::Mat car_image = cv::imread(temp_car + imgtype);
+		file_read(temp_car, txttype);
 
-										//mask
-										cv::Mat mask_image = cv::imread(temp_car + mask + imgtype);
+		//first number
+		int a = ranNum() % num_count;
+		temp_num = directory + number[a] + imgtype;
+		logo_num = cv::imread(temp_num);
+		cv::Mat imageROI = image(cv::Rect(char_x[0], char_y, logo_num.cols, logo_num.rows));
+		cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
 
-										cv::Mat dst1;
-										cv::resize(image, dst1, plate_size, 0, 0);
-										imageROI = car_image(cv::Rect(plate_x[0], plate_y[0], dst1.cols, dst1.rows));
-										cv::addWeighted(imageROI, 0.0, dst1, 1.0, 0., imageROI);
-										
-										cv::Mat dst3;
-										cv::Size back_size = cv::Size(background_image.cols, background_image.rows);
-										cv::resize(background_image, dst3, back_size, 0, 0);
-										imageROI = dst3(cv::Rect(0, 0, background_image.cols, background_image.rows));
-										cv::addWeighted(imageROI, 0.0, dst3, 1.0, 0., imageROI);
+		//second number
+		int b = ranNum() % num_count;
+		temp_num = directory + number[b] + imgtype;
+		logo_num = cv::imread(temp_num);
+		imageROI = image(cv::Rect(char_x[1], char_y, logo_num.cols, logo_num.rows));
+		cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
 
-										/*
-										cv::Mat dst2;
-										cv::Size car_size = cv::Size(car_image.cols, car_image.rows);
-										cv::resize(car_image, dst2, car_size, 0, 0);
-										imageROI = dst3(cv::Rect(0, 0, dst2.cols, dst2.rows));
-										cv::addWeighted(imageROI, 0.0, dst2, 1.0, 0., imageROI);
-										*/
+		//character
+		int c = ranNum() % char_count;
+		temp_num = directory + number[c] + imgtype;
+		logo_char = cv::imread(temp_char);
+		imageROI = image(cv::Rect(char_x[2], char_y, logo_char.cols, logo_char.rows));
+		cv::addWeighted(imageROI, 0.0, logo_char, 1.0, 0., imageROI);
 
-										dst3 = alpha(car_image, dst3, mask_image);
-										dst3 = gaussian(dst3);
-										dst3 = argument(dst3, plate_x, plate_y);
+		//third number
+		int d = ranNum() % num_count;
+		temp_num = directory + number[d] + imgtype;
+		logo_num = cv::imread(temp_num);
+		imageROI = image(cv::Rect(char_x[3], char_y, logo_num.cols, logo_num.rows));
+		cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
 
-										makexml(result_directory, result_name, imgtype, "1" ,background_image.cols, background_image.rows, plate_x[0], plate_x[1], plate_y[0], plate_y[1]);
-										result_name = result_directory + result_name + imgtype;
-										const char * imagefile = result_name.c_str();
-										//cvSaveImage(imagefile, &(IplImage(image)));
-										cvSaveImage(imagefile, &(IplImage(dst3)));
-										cout << a << b << c << d << e << f << g << "번째" << endl;
-										cvDestroyWindow(imagefile);
-										//cvReleaseImage(&(IplImage(background_image)));
+		//fourth number
+		int e = ranNum() % num_count;
+		temp_num = directory + number[e] + imgtype;
+		logo_num = cv::imread(temp_num);
+		imageROI = image(cv::Rect(char_x[4], char_y, logo_num.cols, logo_num.rows));
+		cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
 
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+		//fifth number
+		int f = ranNum() % num_count;
+		temp_num = directory + number[f] + imgtype;
+		logo_num = cv::imread(temp_num);
+		imageROI = image(cv::Rect(char_x[5], char_y, logo_num.cols, logo_num.rows));
+		cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
+
+		//sixth number
+		int g = ranNum() % num_count;
+		temp_num = directory + number[g] + imgtype;
+		logo_num = cv::imread(temp_num);
+		imageROI = image(cv::Rect(char_x[6], char_y, logo_num.cols, logo_num.rows));
+		// 영상 ROI 정의
+		// 사각형으로 시작x 시작y 끝x 끝y
+		cv::addWeighted(imageROI, 0.0, logo_num, 1.0, 0., imageROI);
+		// 영상에 로고 붙이기
+		// imageROI = 1.0*imageROI + 0.3*logo + 0
+		result_name = number[a] + number[b] + character[c] + number[d] + number[e] + number[f] + number[g] + car + char(car_index + '0') + background + char(back_index + '0'); //car number back number
+																																												//cv::imshow("aaa", image);
+		cout << result_name << endl;
+		//cv::imwrite("abc.jpg", image);
+
+		//mask
+		cv::Mat mask_image = cv::imread(temp_car + mask + imgtype);
+
+		cv::Mat dst1;
+		cv::resize(image, dst1, plate_size, 0, 0);
+		imageROI = car_image(cv::Rect(plate_x[0], plate_y[0], dst1.cols, dst1.rows));
+		cv::addWeighted(imageROI, 0.0, dst1, 1.0, 0., imageROI);
+
+		cv::Mat dst3;
+		cv::Size back_size = cv::Size(background_image.cols, background_image.rows);
+		cv::resize(background_image, dst3, back_size, 0, 0);
+		imageROI = dst3(cv::Rect(0, 0, background_image.cols, background_image.rows));
+		cv::addWeighted(imageROI, 0.0, dst3, 1.0, 0., imageROI);
+
+		/*
+		cv::Mat dst2;
+		cv::Size car_size = cv::Size(car_image.cols, car_image.rows);
+		cv::resize(car_image, dst2, car_size, 0, 0);
+		imageROI = dst3(cv::Rect(0, 0, dst2.cols, dst2.rows));
+		cv::addWeighted(imageROI, 0.0, dst2, 1.0, 0., imageROI);
+		*/
+
+
+		int car_location[2] = {ranNum() % (background_image.cols - car_image.cols), ranNum() % (background_image.rows - car_image.rows)};
+
+		dst3 = alpha(car_image, dst3, mask_image, car_location);
+		dst3 = gaussian(dst3);
+		dst3 = argument(dst3, plate_x, plate_y, result_x, result_y);
+
+		makexml(result_directory, result_name, imgtype, "1", background_image.cols, background_image.rows, result_x[0], result_x[1], result_y[0], result_y[1]);
+		result_name = result_directory + result_name + imgtype;
+		const char * imagefile = result_name.c_str();
+		//cvSaveImage(imagefile, &(IplImage(image)));
+		cvSaveImage(imagefile, &(IplImage(dst3)));
+		cout << a << b << c << d << e << f << g << "번째" << endl;
+		cvDestroyWindow(imagefile);
+		//cvReleaseImage(&(IplImage(background_image)));
 	}
 
 	cv::waitKey(0);
